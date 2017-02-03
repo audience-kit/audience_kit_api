47d2fbc58df742d0051fba6f79d85263e64d95d9
Rick Mark <rickmark@outlook.com>
2017-02-02 21:07:41 -0500

    fix locaitons

--- db/migrate/20170203015142_add_user_location.rb
+++ db/migrate/20170203015142_add_user_location.rb
@@ -1,10 +1,11 @@
 class AddUserLocation < ActiveRecord::Migration[5.0]
   def change
-    create_table do |table|
+    create_table :user_locations do |table|
       table.timestamps
 
-      table.geography :location
-      table.references :user
+      table.references :users
     end
+
+    add_column :user_locations, :location, type: :geography
   end
 end
