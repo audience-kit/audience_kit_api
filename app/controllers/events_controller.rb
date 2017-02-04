ce8ed9a276cc0f612a66c0357bb68146199fdade
Rick Mark <rickmark@outlook.com>
2017-02-03 22:04:09 -0500

    Efficiant loading of events for venues

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,11 +6,8 @@ class EventsController < ApplicationController
     if params[:venue_id]
       @events = Venue.find(params[:venue_id]).events
     else
-      if params[:locale_id]
-        @events = Event.joins(:venue).where(venues: { locale_id: params[:locale_id]})
-      else
-        @events = Event.includes(:venue)
-      end
+      @events = Event.includes(:venue)
+      @events = @events.where(venues: { locale_id: params[:locale_id]}) if params[:locale_id]
     end
 
     @events = @events.where('start_at > ?', DateTime.now).order(start_at: :asc)
