16c6d69fb7e784f132dd052bc4fc4bc4dad18aab
Rick Mark <rickmark@outlook.com>
2017-02-12 20:05:25 -0500

    Add missing migration

---
+++ db/migrate/20170212220005_add_people.rb
@@ -0,0 +1,15 @@
+class AddPeople < ActiveRecord::Migration[5.0]
+  def change
+    create_table :people, id: :uuid do |t|
+      t.timestamps
+
+      t.references  :locale
+      t.bigint      :facebook_id, null: false
+      t.jsonb       :facebook_graph
+      t.datetime    :facebook_updated_at
+      t.references  :venue
+      t.string      :display_name
+      t.string      :facebook_token
+    end
+  end
+end
