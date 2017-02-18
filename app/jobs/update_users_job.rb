6c1a643cc2b669a870d3e60681330b54bf3c34e5
Rick Mark <rickmark@outlook.com>
2017-02-18 08:24:45 -0500

    Bug in universal people

--- app/jobs/update_users_job.rb
+++ app/jobs/update_users_job.rb
@@ -3,7 +3,7 @@ class UpdateUsersJob < ApplicationJob
   def perform
     users = User.where { |u| u.facebook_token != nil }
 
-    users.for_each do |user|
+    users.each do |user|
       graph = Koala::Facebook::API.new user.facebook_token
 
       user.update_from graph.get_object '/me'
