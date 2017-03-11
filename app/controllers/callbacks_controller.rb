d909eea493e9040386c6925a3579e2ed62aa9673
Rick Mark <rickmark@outlook.com>
2017-03-11 08:22:35 -0700

    Verification for webhooks

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -7,6 +7,14 @@ class CallbacksController < ApplicationController
     logger.info params.inspect
   end
 
+  def facebook_verify
+    if params['hub.mode'] == 'subscribe' and params['hub.verify_token'] == Rails.application.secrets.facebook_callback_secret
+      render text: params['hub.challenge']
+    else
+      render status: :bad_request
+    end
+  end
+
   # Delete all user content
   def facebook_deauthorize
 
