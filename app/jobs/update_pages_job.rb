8c74962da286ee88ccb30f957de1c3a7e5f05e9e
Rick Mark <rickmark@outlook.com>
2017-03-25 16:41:32 -0600

    gotta save it duh

--- app/jobs/update_pages_job.rb
+++ app/jobs/update_pages_job.rb
@@ -88,9 +88,9 @@ class UpdatePagesJob < ApplicationJob
         puts "No venue for #{event_graph['name']} (#{event_graph['id']})"
       end
 
-      HotMessModels::EventPerson.find_or_create_by(person: page.person, event: event_model, role: 'host') if page.person
+      event_model.update_details_from_facebook
 
-      event_model.update_details_from_facebook if event_model.venue
+      HotMessModels::EventPerson.find_or_create_by(person: page.person, event: event_model, role: 'host') if page.person
     rescue => ex
       puts "Error updating object #{page.name}'s event #{ex}"
     end
