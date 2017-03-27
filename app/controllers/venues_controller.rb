77a088296f1a1dd7a10354368da78677b985d870
Rick Mark <rickmark@outlook.com>
2017-03-26 18:02:22 -0600

    join the right way

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -72,7 +72,7 @@ class VenuesController < ApplicationController
   end
 
   def photo
-    @venue = HotMessModels::Venue.find(params[:id]).include(:location)
+    @venue = HotMessModels::Venue.includes(location: :photo).find(params[:id])
 
     if @venue.location.photo
       return redirect_to "/photos/#{@venue.location.photo.id}"
