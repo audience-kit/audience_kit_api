9474f74394f9bef77e4fe7aa906236bdedb49f7b
Rick Mark <rickmark@outlook.com>
2017-04-08 11:08:59 -0700

    First Alexa response

--- app/controllers/alexa_controller.rb
+++ app/controllers/alexa_controller.rb
@@ -2,6 +2,8 @@ class AlexaController < ApplicationController
   skip_before_action :authenticate
 
   def index
-    render text: 'Hello from rails'
+    @events = Event.future.take(3)
+
+    render text: "The next few events are #{events.map(&:name).to_sentence}"
   end
 end
