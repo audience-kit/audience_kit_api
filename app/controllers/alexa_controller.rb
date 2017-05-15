76338ed965acc50ab1132a8bc3b688df308285f8
Rick Mark <rickmark@outlook.com>
2017-05-15 10:12:37 -0700

    First pass alexa2

--- app/controllers/alexa_controller.rb
+++ app/controllers/alexa_controller.rb
@@ -1,7 +1,7 @@
 class AlexaController < ApplicationController
   skip_before_action :authenticate
 
-  #before_action :alexa_authenticate
+  before_action :alexa_authenticate
 
   def events
     location = Geocoder.search(params[:zip]).first
@@ -15,7 +15,8 @@ class AlexaController < ApplicationController
     render json: { locale: locale.name, events: @events.map { |event| {
         title: event.name,
         venue: event.venue.name,
-        start_at: event.start_at
+        start_at: event.start_at,
+        timezone_delta: locale.timezone_zulu_delta
     } } }
   end
 
@@ -25,6 +26,8 @@ class AlexaController < ApplicationController
 
   private
   def alexa_authenticate
+    key = /Bearer (.*)/.match(request.authorization)
 
+    return render status: 401 unless key[0] == Rails.application.secrets.alexa_woker_key
   end
 end
