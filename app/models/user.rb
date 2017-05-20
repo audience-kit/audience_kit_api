e9f628d67d288e8ac697a605cdbb1fddccfd9f45
Rick Mark <rickmark@outlook.com>
2017-05-19 23:20:57 -0700

    Text on signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,18 @@ class User < ApplicationRecord
         u.gender = graph['gender']
 
         u.facebook_graph = graph
+
+        begin
+          client = Twilio::REST::Client.new 'AC5f75bb86a003e5cda83d9d7514de864b', Rails.application.secrets[:twilio_key]
+
+          client.messages.create(
+            from: '+14063154776',
+            to: '+12069133215',
+            body: "New User: #{u.first_name} #{u.last_name}"
+          )
+        rescue => ex
+          logger.error ex
+        end
       end
     end
 
