50e35fe1c1b5fad4f22a0a7a58f1a67dd481e755
Rick Mark <rickmark@outlook.com>
2017-05-20 20:00:38 -0700

    Client optional

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,8 +1,9 @@
-TWILIO_CLIENT = Twilio::REST::Client.new 'AC5f75bb86a003e5cda83d9d7514de864b', Rails.application.secrets[:twilio_key]
 
 def send_text_message(text)
   begin
-    TWILIO_CLIENT.messages.create(
+    client = Twilio::REST::Client.new 'AC5f75bb86a003e5cda83d9d7514de864b', Rails.application.secrets[:twilio_key]
+
+    client.messages.create(
         from: '+14063154776',
         to: '+12069133215',
         body: "New User: #{u.first_name} #{u.last_name}"
