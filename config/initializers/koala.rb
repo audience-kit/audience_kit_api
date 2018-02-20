301517dac02ac89b873d2ce1249d5512ed69c271
Rick Mark <rickmark@outlook.com>
2018-02-19 16:51:46 -0800

    Ensure google api key from secrets

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -2,7 +2,7 @@
 Koala.configure do |config|
   config.app_id = ::Rails.application.secrets[:facebook_app_id]
   config.app_secret = ::Rails.application.secrets[:facebook_secret]
-  config.api_version = 'v2.9'
+  config.api_version = 'v2.12'
   # See Koala::Configuration for more options, including details on how to send requests through
   # your own proxy servers.
 end
