9b75d51c73a7a0742b5d08c7e986a4e9d3c091a9
Rick Mark <rickmark@outlook.com>
2018-03-28 17:38:26 -0500

    Second take

--- app/jobs/update_pages_job.rb
+++ app/jobs/update_pages_job.rb
@@ -1,10 +1,10 @@
 class UpdatePagesJob < ApplicationJob
   def perform
-    Venue.where(hidden: false).order(:updated_at).includes(:page).each do |venue|
+    Venue.where(hidden: false).order("pages.updated_at").includes(:page).each do |venue|
       VenueUpdater.new(venue).update
     end
 
-    Person.includes(:page).order(:updated_at).each do |person|
+    Person.includes(:page).order("pages.updated_at").each do |person|
       PersonUpdater.new(person).update
     end
   end
